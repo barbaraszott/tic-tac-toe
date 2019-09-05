cd2a4645bf13d3bea80c09fec49891e3df9f715c
Barbara Szott <barbara.aleksy@gmail.com>
2019-09-05 15:10:25 +0200

    Create backbone of main component Game.js

---
+++ src/Game.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Game.scss';
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			game       : new Array(9).fill(null),
+			player     : null,
+			isFinished : false
+		};
+	}
+
+	render() {
+		return (
+			<div className="game">
+				<h1>Tic Tac Toe</h1>
+				<h2>CurrentPlayer</h2>
+				<span>{this.state.player}</span>
+			</div>
+		);
+	}
+}
+
+export default Game;
